462a90d1d0ef655594c24d5d265a7a3cc8b0e657
DoubleHH <huanghui2008123@163.com>
2014-03-15 22:16:01 +0800

    A Running Game developed by Cocos2d-x
    As a learn project of Cocos2d-x, Thank the video author (龙灵修) at
    www.youku.com

---
+++ HPaoKuGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  HPaoKuGameAppController.h
+//  HPaoKuGame
+//
+//  Created by DoubleHH on 14-3-6.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
